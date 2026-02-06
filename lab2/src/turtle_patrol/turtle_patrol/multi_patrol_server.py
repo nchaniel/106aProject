b6c2af49a1ff2db1f969d7bea2ea241aa3c7c93e
Nathan L <nathan.k.lam04@gmail.com>
2026-02-05 17:07:10 -0800

    more comments

--- lab2/src/turtle_patrol/turtle_patrol/multi_patrol_server.py
+++ lab2/src/turtle_patrol/turtle_patrol/multi_patrol_server.py
@@ -16,69 +16,60 @@ class MultiTurtlePatrolServer(Node):
             self.patrol_callback
         )
 
-        # turtle_name -> { pub, teleport_client, vel, omega }
+        # turtle_name -> { pub, teleport_client, vel, omega }; stores state of each turtle  
         self._turtles = {}
 
         # Timer publishes cmd_vel for all turtles
         self.create_timer(0.1, self.publish_all_cmds)
 
-        self.get_logger().info("MultiTurtlePatrolServer ready.")
-
     # --------------------------------------------------
-    # Timer callback
+    # Timer publishes current Twist
     # --------------------------------------------------
     def publish_all_cmds(self):
-        for data in self._turtles.values():
+        for turtle in self._turtles.values(): 
             msg = Twist()
-            msg.linear.x = data['vel']
-            msg.angular.z = data['omega']
-            data['pub'].publish(msg)
+            msg.linear.x = turtle['vel']
+            msg.angular.z = turtle['omega']
+            turtle['pub'].publish(msg) #actually moves turtle by publishing to cmd_vel topic
 
     # --------------------------------------------------
-    # Service callback (FIXED)
+    # Service callback: update speeds and teleport; if new turtle, set up publisher and teleport client for it
     # --------------------------------------------------
-    def patrol_callback(self, request, response):
+    def patrol_callback(self, request, response): #recieves request from client, sends response back to client
         name = request.turtle_name
 
-        self.get_logger().info(
-            f"{name}: teleport to ({request.x}, {request.y}, {request.theta}), "
-            f"vel={request.vel}, omega={request.omega}"
-        )
-
-        # First time this turtle is seen
+        # if first time this turtle is seen
         if name not in self._turtles:
-            pub = self.create_publisher(Twist, f'/{name}/cmd_vel', 10)
-
+            pub = self.create_publisher(Twist, f'/{name}/cmd_vel', 10) #publisher for this turtle
+            
+            #creates teleport client to call the teleport service
             teleport_client = self.create_client(
                 TeleportAbsolute,
                 f'/{name}/teleport_absolute'
-            )
+            ) 
 
-            if not teleport_client.wait_for_service(timeout_sec=2.0):
+            if not teleport_client.wait_for_service(timeout_sec=2.0): #if turtle doesn't exist, return failure response
                 response.success = False
                 response.message = f"Teleport service not available for {name}"
-                return response
+                return response     
 
-            self._turtles[name] = {
+            self._turtles[name] = {  #store info needed to control turtle 
                 'pub': pub,
                 'teleport_client': teleport_client,
                 'vel': 0.0,
                 'omega': 0.0
             }
 
-        # -------------------------------
-        # Teleport asynchronously
-        # -------------------------------
+        # creates the teleport request
         teleport_req = TeleportAbsolute.Request()
+        # put info for teleport request
         teleport_req.x = request.x
         teleport_req.y = request.y
         teleport_req.theta = request.theta
 
-        self._turtles[name]['teleport_client'].call_async(teleport_req)
+        self._turtles[name]['teleport_client'].call_async(teleport_req) #send teleport request
 
-        # -------------------------------
-        # Store patrol parameters
-        # -------------------------------
+        #update turtle with requested values
         self._turtles[name]['vel'] = request.vel
         self._turtles[name]['omega'] = request.omega
 
