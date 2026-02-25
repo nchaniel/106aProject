08f8b537391f9f15a8380a1aeba0283249957634
Nathan <nathanielchan@berkeley.edu>
2026-02-25 14:27:01 -0800

    checkoff 1 done

--- lab3/src/forward_kinematics/forward_kinematics/forward_kinematics_node.py
+++ lab3/src/forward_kinematics/forward_kinematics/forward_kinematics_node.py
@@ -4,7 +4,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
 import numpy as np
-from forward_kinematics import ur7e_forward_kinematics_from_angles
+from forward_kinematics.forward_kinematics import ur7e_forward_kinematics_from_angles
 
 class ForwardKinematicsNode(Node):
     def __init__(self):
@@ -24,17 +24,27 @@ class ForwardKinematicsNode(Node):
         Callback function that is called when a new /joint_states message is received.
         It extracts joint angles from the message, computes the forward kinematics, and prints the resulting transformation matrix.
         """
-        joint_angles = msg.position  # Joint angles from the /joint_states message
+        joint_dict = dict(zip(msg.name, msg.position))
 
-        # Ensure we have exactly 6 joint angles (as expected for UR7e)
-        if len(joint_angles) == 6:
-            # Compute the transformation matrix
-            transformation_matrix = ur7e_forward_kinematics_from_angles(np.array(joint_angles))
+        try:
+            # re-order the joints explicitly as /joint_states topic typically publishes joint data in alphabetical order, apparently
+            ordered_angles = np.array([
+                joint_dict['shoulder_pan_joint'],
+                joint_dict['shoulder_lift_joint'],
+                joint_dict['elbow_joint'],
+                joint_dict['wrist_1_joint'],
+                joint_dict['wrist_2_joint'],
+                joint_dict['wrist_3_joint']
+            ])
 
-            # Print the resulting transformation matrix
-            self.get_logger().info(f"Transformation Matrix (for End-Effector):\n{transformation_matrix}")
-        else:
-            self.get_logger().warn("Expected 6 joint angles.")
+            # calculate transformation matrix
+            transformation_matrix = ur7e_forward_kinematics_from_angles(ordered_angles)
+
+            # print resulting transformation matrix
+            self.get_logger().info(f"Transformation Matrix:\n{transformation_matrix}")
+
+        except KeyError as e:
+            self.get_logger().warn(f"Missing joint in /joint_states: {e}")
 
 def main(args=None):
     rclpy.init(args=args)
